3f5f0399da1b86174e37dc54b56097d97d5fc5bb
Sri Harsha Gangisetty <130010051@iitb.ac.in>
2016-10-20 02:07:19 +0530

    GUI elements added

---
+++ iris.py
@@ -0,0 +1,32 @@
+from PyQt4 import QtGui, uic
+import sys
+
+iris_uic = uic.loadUiType('gui/iris.ui')[0]
+
+class IrisGui(QtGui.QMainWindow,iris_uic):
+
+	def __init__(self):
+		QtGui.QMainWindow.__init__(self)
+		self.setupUi(self)
+		self.intialize()
+
+	def intialize(self):
+		self.select_file_button.clicked.connect(self.selectFileButtonClicked)
+		self.get_relations_button.clicked.connect(self.getRelationsButtonClicked)
+
+	def selectFileButtonClicked(self):
+		self.file_select_dialog = QtGui.QFileDialog()
+		self.file_select_dialog.show()
+		
+
+	def getRelationsButtonClicked(self):
+		pass
+
+def main():
+	app = QtGui.QApplication(sys.argv)
+	iris = IrisGui()
+	iris.show()
+	app.exec_()
+
+if __name__=="__main__":
+	main()
